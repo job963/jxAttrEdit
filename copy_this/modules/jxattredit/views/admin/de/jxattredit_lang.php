e1dc81ac544114a6de54f2bae73ad41215787c27
Joachim Barthel <jobarthel@gmail.com>
2014-11-20 20:48:00 +0100

    Version number increased

--- copy_this/modules/jxattredit/views/admin/de/jxattredit_lang.php
+++ copy_this/modules/jxattredit/views/admin/de/jxattredit_lang.php
@@ -1,5 +1,7 @@
 <?php
+
 $nPathEnd = strpos( $_SERVER['SCRIPT_FILENAME'], '/admin/index.php' );
 $sShopPath = substr( $_SERVER['SCRIPT_FILENAME'], 0, $nPathEnd );
 include $sShopPath . '/modules/jxattredit/application/views/admin/de/jxattredit_lang.php';
+
 ?>
