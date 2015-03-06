f71063ebef3dbf1efcfaaa6c8aea77c7915d1d0f
Joachim Barthel <jobarthel@gmail.com>
2015-03-06 21:33:20 +0100

    Correction of path for including language files

--- copy_this/modules/jxattredit/metadata.php
+++ copy_this/modules/jxattredit/metadata.php
@@ -16,7 +16,7 @@ $aModule = array(
                         'en' => 'Attribute Editor for Products.'
                         ),
     'thumbnail'    => 'jxattredit.png',
-    'version'      => '0.4.3',
+    'version'      => '0.4.4',
     'author'       => 'Joachim Barthel',
     'url'          => 'https://github.com/job963/jxAttrEdit',
     'email'        => 'jobarthel@gmail.com',
--- copy_this/modules/jxattredit/views/admin/en/jxattredit_lang.php
+++ copy_this/modules/jxattredit/views/admin/en/jxattredit_lang.php
@@ -1,7 +1,8 @@
 <?php
 
-$nPathEnd = strpos( $_SERVER['SCRIPT_FILENAME'], '/admin/index.php' );
+/*$nPathEnd = strpos( $_SERVER['SCRIPT_FILENAME'], '/admin/index.php' );
 $sShopPath = substr( $_SERVER['SCRIPT_FILENAME'], 0, $nPathEnd );
-include $sShopPath . '/modules/jxattredit/application/views/admin/en/jxattredit_lang.php';
+include $sShopPath . '/modules/jxattredit/application/views/admin/en/jxattredit_lang.php';*/
+include oxRegistry::get( "oxConfigFile" )->getVar( "sShopDir" ) . '/modules/jxattredit/application/views/admin/en/jxattredit_lang.php';
 
 ?>
